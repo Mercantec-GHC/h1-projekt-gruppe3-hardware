c5161aa94ac96d00da500eb3eacc3db69b59dc19
Zer0ByteGitHub <95279260+Zer0ByteGitHub@users.noreply.github.com>
2023-12-04 04:22:09 -0800

    Update script.sql

--- SQL-Scripts/script.sql
+++ SQL-Scripts/script.sql
@@ -1,2 +1,32 @@
 --@block Her kan i skrive SQL scripts til at oprette jeres DB
-SELECT * FROM sql
+CREATE TABLE [dbo].[ShoppingCartItem]
+(
+	ShoppingCartItemID INT PRIMARY KEY IDENTITY(1,1),
+    SellerID INT,
+    HardwareID INT,
+    FOREIGN KEY (SellerID) REFERENCES Seller(SellerID),
+    FOREIGN KEY (HardwareID) REFERENCES Hardware(HardwareID)
+)
+
+CREATE TABLE [dbo].[Hardware]
+(
+	HardwareID INT PRIMARY KEY IDENTITY(1,1),
+    Type NVARCHAR(255) NOT NULL,
+    Name NVARCHAR(255) NOT NULL,
+    AttackSurface NVARCHAR(255),
+    State NVARCHAR(255) NOT NULL,
+    Version DECIMAL(5,2) NOT NULL,
+    Description NVARCHAR(255),
+    Price DECIMAL(10,2) NOT NULL,
+    SellerID INT,
+    FOREIGN KEY (SellerID) REFERENCES Seller(SellerID)
+)
+
+CREATE TABLE [dbo].[Seller]
+(
+	SellerID INT PRIMARY KEY IDENTITY(1,1),
+    Name NVARCHAR(255) NOT NULL,
+    PhoneNumber NVARCHAR(20) NOT NULL,
+    Email NVARCHAR(255) NOT NULL,
+    Trusted BIT NOT NULL
+)
