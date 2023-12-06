8dccd914c9d63204fd3732ccfa1a3d756633c6dc
Tobias <115145472+TobiasDK22@users.noreply.github.com>
2023-12-06 18:28:11 +0100

    SQL kode og DB kode i Visual Studio
    I SQL Database koden er der blevet ændret lidt, så de er NOT NULL.
    
    I Database koden fra Visual Studio er der blevet indsat notater, for at vi kan holde styr på hvad diverse ting i koden gør

--- BlazorApp/Pages/DbContext.cs
+++ BlazorApp/Pages/DbContext.cs
@@ -5,15 +5,19 @@ namespace BlazorApp.Pages
 {
     public class AppDbContext : DbContext
     {
+        //Samling af brugerobjekter i databasen
         public DbSet<Seller> Users { get; set; }
+        //Samling af produkter i databasen
         public DbSet<Hardware> Products { get; set; }
 
+        //Forbindelse til databasen
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            //Fortæller det er en SQL Database
             optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Local;");
         }
     }
--- SQL-Scripts/script.sql
+++ SQL-Scripts/script.sql
@@ -2,9 +2,9 @@
 CREATE TABLE Users
 (
     Id INT PRIMARY KEY IDENTITY(1,1),
-    Name NVARCHAR(255),
-    PhoneNumber NVARCHAR(20),
-    Email NVARCHAR(255),
+    Name NVARCHAR(255) NOT NULL, 
+    PhoneNumber NVARCHAR(20) NOT NULL, 
+    Email NVARCHAR(255) NOT NULL, 
     Trusted BIT,
     ProductId INT FOREIGN KEY REFERENCES Products(Id)
 );
@@ -13,11 +13,11 @@ CREATE TABLE Users
 CREATE TABLE Products
 (
     Id INT PRIMARY KEY IDENTITY(1,1),
-    Type NVARCHAR(50),
-    Name NVARCHAR(255),
+    Type NVARCHAR(50) NOT NULL,
+    Name NVARCHAR(255) NOT NULL,
     AttackSurface NVARCHAR(255),
-    State NVARCHAR(255),
+    State NVARCHAR(255) NOT NULL,
     Version FLOAT,
-    Description NVARCHAR(MAX),
-    Price DECIMAL(18,2)
+    Description NVARCHAR(MAX) NOT NULL,
+    Price DECIMAL(18,2) NOT NULL, 
 );
