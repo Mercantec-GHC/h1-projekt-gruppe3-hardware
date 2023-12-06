4b105401628d6f197bb80cbafecc2ec7ddecf4dc
Tobias <115145472+TobiasDK22@users.noreply.github.com>
2023-12-06 18:17:44 +0100

    Ændret forside, og kontakt side
    Jeg har ændret design på forsiden. Der er kommet samme gradient baggrund som i sidebaren/navmenuen, så det passer lidt mere sammen. Billeder er blevet ændret til brugte ting, Price teskten er blevet ændret til gradient også. I kontaktformularen er baggrunden også ændret, og tekst er passet til, så det virker ordentligt

--- SQL-Scripts/script.sql
+++ SQL-Scripts/script.sql
@@ -1,33 +1,23 @@
---@block Her kan i skrive SQL scripts til at oprette jeres DB
-
-CREATE TABLE [dbo].[Seller]
+-- Opret tabel for brugere
+CREATE TABLE Users
 (
-	SellerID INT PRIMARY KEY IDENTITY(1,1),
-    Name NVARCHAR(255) NOT NULL,
-    PhoneNumber NVARCHAR(20) NOT NULL,
-    Email NVARCHAR(255) NOT NULL,
-    Trusted BIT NOT NULL
-)
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    Name NVARCHAR(255),
+    PhoneNumber NVARCHAR(20),
+    Email NVARCHAR(255),
+    Trusted BIT,
+    ProductId INT FOREIGN KEY REFERENCES Products(Id)
+);
 
-CREATE TABLE [dbo].[Hardware]
+-- Opret tabel for produkter
+CREATE TABLE Products
 (
-	HardwareID INT PRIMARY KEY IDENTITY(1,1),
-    Type NVARCHAR(255) NOT NULL,
-    Name NVARCHAR(255) NOT NULL,
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    Type NVARCHAR(50),
+    Name NVARCHAR(255),
     AttackSurface NVARCHAR(255),
-    State NVARCHAR(255) NOT NULL,
-    Version DECIMAL(5,2) NOT NULL,
-    Description NVARCHAR(255),
-    Price DECIMAL(10,2) NOT NULL,
-    SellerID INT,
-    FOREIGN KEY (SellerID) REFERENCES Seller(SellerID)
-)
-
-CREATE TABLE [dbo].[ShoppingCartItem]
-(
-	ShoppingCartItemID INT PRIMARY KEY IDENTITY(1,1),
-    SellerID INT,
-    HardwareID INT,
-    FOREIGN KEY (SellerID) REFERENCES Seller(SellerID),
-    FOREIGN KEY (HardwareID) REFERENCES Hardware(HardwareID)
-)
+    State NVARCHAR(255),
+    Version FLOAT,
+    Description NVARCHAR(MAX),
+    Price DECIMAL(18,2)
+);
